de0744be788138c07769569c97ca23a2e403882a
Rubén Zagon <ruben.zagon@gmail.com>
2022-04-05 07:58:10 +0100

    🎉 first commit

---
+++ with-springboot-and-jdbcTemplate/src/main/java/com/leanmind/withspringbootandjdbctemplate/WithSpringbootAndJdbcTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.leanmind.withspringbootandjdbctemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithSpringbootAndJdbcTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithSpringbootAndJdbcTemplateApplication.class, args);
+	}
+
+}
---
+++ with-springboot-and-jdbcTemplate/src/test/java/com/leanmind/withspringbootandjdbctemplate/WithSpringbootAndJdbcTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leanmind.withspringbootandjdbctemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithSpringbootAndJdbcTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
