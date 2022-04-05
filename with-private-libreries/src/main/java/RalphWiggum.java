a24680d63b5d984cf37093f3e038ca1b5366b45a
Rubén Zagon <ruben.zagon@gmail.com>
2022-04-05 08:51:47 +0100

    ✨ feature: Add Ralph Wiggum

---
+++ with-private-libreries/src/main/java/RalphWiggum.java
@@ -0,0 +1,8 @@
+final public class RalphWiggum {
+    private final String name = "Ralph";
+    private final String lastName = "Wiggum";
+
+    public static String saySomethingSmart() {
+        return "¡Bebo jugo azul debajo del fregadero!";
+    }
+}
