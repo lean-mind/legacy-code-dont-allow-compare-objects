212c8f9bdc7f2ae20cd207b9246ca3e131c8604f
Rubén Zagon <ruben.zagon@gmail.com>
2022-08-26 13:49:15 +0200

    🚚 Move files to root directory

--- with-private-libreries/src/main/java/RalphWiggum.java
+++
@@ -1,8 +0,0 @@
-final public class RalphWiggum {
-    private final String name = "Ralph";
-    private final String lastName = "Wiggum";
-
-    public static String saySomethingSmart() {
-        return "¡Bebo jugo azul debajo del fregadero!";
-    }
-}
